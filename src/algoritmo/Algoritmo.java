c311ce388996c2ddd0b05e9a7591faa70f47a8d0
kaykydlima <kayky140703@gmail.com>
2021-08-22 21:21:11 -0300

    algoritmo

---
+++ src/algoritmo/Algoritmo.java
@@ -0,0 +1,19 @@
+
+package algoritmo;
+
+public class Algoritmo {
+
+    public static void main(String[] args) {
+        //Declaração váriavel com valor "X" e "Y"
+        String letra = "X";
+        String letra2 = "Y";
+        //Input das váriaveis
+        System.out.println(letra);
+        System.out.println(letra2);
+        //Concatenação das váriaveis com valores "X" e "Y"
+        String s = letra + letra2;
+        //Exibição da concatenação
+        System.out.println(s);
+    }
+    
+}
